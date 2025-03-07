49bd468bd08a4f6b8b015d174bd6695b9af5ec3f
Milan Balazs <milanbalazs01@gmail.com>
2025-03-07 19:26:17 +0100

    Split the too long method to smaller methods

--- src/container_simulation/container_analyzer/analyzer.py
+++ src/container_simulation/container_analyzer/analyzer.py
@@ -194,7 +194,8 @@ class ContainerizedSystemAnalyzer:
             "total_tx_mb": round(total_tx, 4),
         }
 
-    def get_mean_values(self, samples: dict) -> dict:
+    @staticmethod
+    def get_mean_values(samples: dict) -> dict:
         """
         Computes the mean values from collected resource usage samples.
 
@@ -220,7 +221,6 @@ class ContainerizedSystemAnalyzer:
 
         return return_structure
 
-    # TODO: This method is too big. Cross-check how can be split it.
     def analyze_container_performance(
         self,
         container_or_service_id: Optional[str] = None,
@@ -229,29 +229,23 @@ class ContainerizedSystemAnalyzer:
         write_to_file: bool = False,
     ) -> dict:
         """
-        Collects resource usage statistics for a Docker container
-            or Swarm service over a time window.
+        Collects resource usage statistics for a Docker container or
+        Swarm service over a time window.
 
         Args:
             container_or_service_id (Optional[str], optional): The container or service ID.
             container_or_service_name (Optional[str], optional): The container or service name.
-            all_entity (bool): If it's True then all the available Docker entity will be
-                analyzed (containers, services).
-            write_to_file (bool): If it's True then the result of the analysis will be written to
-                a Json file.
-                The file will contain all the samples.
-                Important: The file writing is not stream like so it will be generated only in case
-                    of successful execution!
+            all_entity (bool): If True, analyzes all available Docker
+                entities (containers, services).
+            write_to_file (bool): If True, writes the collected analysis data to a JSON file.
 
         Returns:
-            # TODO: Cross-check.
-                It will be changed due to complete system analysis.
             dict: A dictionary containing:
-                - "average_cpu_cores" (float): Average CPU cores used.
-                - "average_ram_usage_mb" (float): Average RAM usage in MB.
-                - "average_disk_usage_mb" (float): Average disk usage in MB.
-                - "average_rx_mb" (float): Average network received data in MB.
-                - "average_tx_mb" (float): Average network transmitted data in MB.
+                - "mean_cpu_cores" (float): Mean CPU cores used.
+                - "mean_ram_usage_mb" (float): Mean RAM usage in MB.
+                - "mean_disk_usage_mb" (float): Mean disk usage in MB.
+                - "mean_rx_mb" (float): Mean network received data in MB.
+                - "mean_tx_mb" (float): Mean network transmitted data in MB.
                 - "samples_cpu" (dict): CPU usage samples over time.
                 - "samples_ram" (dict): RAM usage samples over time.
                 - "samples_disk" (dict): Disk usage samples over time.
@@ -259,121 +253,225 @@ class ContainerizedSystemAnalyzer:
                 - "samples_tx" (dict): Network TX samples over time.
         """
         start_time: float = time.time()
+        previous_stat, samples = self._initialize_sample_storage()
+        analyze_entities = self._get_analyze_entities(
+            container_or_service_id, container_or_service_name, all_entity
+        )
 
-        previous_stat: dict = {}
-        cpu_cores_samples: dict = {}
-        ram_usage_samples: dict = {}
-        disk_usage_samples: dict = {}
-        rx_usage_samples: dict = {}
-        tx_usage_samples: dict = {}
+        sample_tick: float = 0
+        already_analyzed: list[str] = []
 
-        if all_entity:
-            analyze_entities = self.analyzer.get_entity()
-        else:
-            analyze_entities = [
+        while (time.time() - start_time) < self.time_window:
+            self._collect_samples(
+                analyze_entities, already_analyzed, previous_stat, samples, sample_tick
+            )
+            sample_tick += self.period
+            time.sleep(self.period)
+
+        if write_to_file:
+            self._write_results_to_file(samples)
+
+        return self._compute_results(samples)
+
+    @staticmethod
+    def _initialize_sample_storage() -> tuple[dict, dict]:
+        """
+        Initializes the storage dictionaries for previous stats and resource usage samples.
+
+        Returns:
+            tuple: A tuple containing:
+                - previous_stat (dict): Stores previous statistical data for each entity.
+                - samples (dict): Stores the collected CPU, RAM, Disk, and Network samples.
+        """
+        return {}, {
+            "cpu_cores_samples": {},
+            "ram_usage_samples": {},
+            "disk_usage_samples": {},
+            "rx_usage_samples": {},
+            "tx_usage_samples": {},
+        }
+
+    def _get_analyze_entities(
+        self,
+        container_or_service_id: Optional[str],
+        container_or_service_name: Optional[str],
+        all_entity: bool,
+    ) -> list:
+        """
+        Retrieves the list of entities to analyze.
+
+        Args:
+            container_or_service_id (Optional[str]): Container/service ID.
+            container_or_service_name (Optional[str]): Container/service name.
+            all_entity (bool): If True, retrieves all available entities.
+
+        Returns:
+            list: A list of container or service entities to analyze.
+        """
+        return (
+            self.analyzer.get_entity()
+            if all_entity
+            else [
                 self.analyzer.get_container_or_service_ref(
                     container_or_service_id, container_or_service_name
                 )
             ]
+        )
 
-        sample_tick: float = 0
+    def _collect_samples(
+        self,
+        analyze_entities: list,
+        already_analyzed: list[str],
+        previous_stat: dict,
+        samples: dict,
+        sample_tick: float,
+    ) -> None:
+        """
+        Collects resource usage samples for all entities at a given time tick.
 
-        already_analyzed: list[str] = []
+        Args:
+            analyze_entities (list): List of entities to analyze.
+            already_analyzed (list[str]): List of already analyzed entities.
+            previous_stat (dict): Dictionary storing previous statistics for each entity.
+            samples (dict): Dictionary containing collected samples.
+            sample_tick (float): The current sample time tick.
+        """
+        for docker_entity in analyze_entities:
+            docker_entity_name = docker_entity.name
+            # TODO: Fix the timestamps. The tick usage is not OK.
+            formatted_tick: str = "{:.2f}".format(sample_tick).rstrip("0").rstrip(".")
 
-        # TODO: Fix the timestamp in the structure. The tick is not OK.
+            if docker_entity_name not in already_analyzed:
+                previous_stat[docker_entity_name] = self.analyzer.get_stats(name=docker_entity_name)
+                self._initialize_entity_samples(samples, docker_entity_name)
+                already_analyzed.append(docker_entity_name)
 
-        while (time.time() - start_time) < self.time_window:
-            for docker_entity in analyze_entities:
-                docker_entity_name = docker_entity.name
-                formatted_tick: str = "{:.2f}".format(sample_tick).rstrip("0").rstrip(".")
+            current_stat: dict = self.analyzer.get_stats(name=docker_entity_name)
 
-                # Ensure entity is initialized
-                if docker_entity_name not in already_analyzed:
-                    previous_stat[docker_entity_name] = self.analyzer.get_stats(
-                        name=docker_entity_name
-                    )
+            self._update_samples(
+                samples, docker_entity_name, formatted_tick, current_stat, previous_stat
+            )
 
-                    cpu_cores_samples[docker_entity_name] = {}
-                    ram_usage_samples[docker_entity_name] = {}
-                    disk_usage_samples[docker_entity_name] = {}
-                    rx_usage_samples[docker_entity_name] = {}
-                    tx_usage_samples[docker_entity_name] = {}
+            previous_stat[docker_entity_name] = current_stat
 
-                    already_analyzed.append(docker_entity_name)
+    @staticmethod
+    def _initialize_entity_samples(samples: dict, entity_name: str) -> None:
+        """
+        Initializes the sample dictionaries for a new entity.
 
-                # Get current stats
-                current_stat: dict = self.analyzer.get_stats(name=docker_entity_name)
+        Args:
+            samples (dict): Dictionary storing resource usage samples.
+            entity_name (str): Name of the entity being analyzed.
+        """
+        samples["cpu_cores_samples"][entity_name] = {}
+        samples["ram_usage_samples"][entity_name] = {}
+        samples["disk_usage_samples"][entity_name] = {}
+        samples["rx_usage_samples"][entity_name] = {}
+        samples["tx_usage_samples"][entity_name] = {}
 
-                # CPU and RAM usage
-                cpu_cores_samples[docker_entity_name][formatted_tick] = self.get_cpu_cores_used(
-                    current_stat
-                )
-                ram_usage_samples[docker_entity_name][formatted_tick] = self.get_ram_usage_mb(
-                    current_stat
-                )
+    def _update_samples(
+        self,
+        samples: dict,
+        entity_name: str,
+        formatted_tick: str,
+        current_stat: dict,
+        previous_stat: dict,
+    ) -> None:
+        """
+        Updates the resource usage samples for a given entity at a specific time tick.
 
-                # Disk Usage (instead of R/W speed)
-                disk_usage_samples[docker_entity_name][formatted_tick] = (
-                    self.analyzer.get_disk_usage(container_or_service_name=docker_entity_name)
-                )
+        Args:
+            samples (dict): Dictionary storing collected samples.
+            entity_name (str): The name of the entity being analyzed.
+            formatted_tick (str): The formatted sample tick.
+            current_stat (dict): The current statistics of the entity.
+            previous_stat (dict): The previous statistics of the entity.
+        """
+        samples["cpu_cores_samples"][entity_name][formatted_tick] = self.get_cpu_cores_used(
+            current_stat
+        )
+        samples["ram_usage_samples"][entity_name][formatted_tick] = self.get_ram_usage_mb(
+            current_stat
+        )
+        samples["disk_usage_samples"][entity_name][formatted_tick] = self.analyzer.get_disk_usage(
+            container_or_service_name=entity_name
+        )
 
-                # Extract network stats
-                networks_current = current_stat.get("networks", {})
-                networks_previous = previous_stat[docker_entity_name].get("networks", {})
+        self._update_network_samples(
+            samples, entity_name, formatted_tick, current_stat, previous_stat
+        )
 
-                # Auto-detect the first available network interface
-                network_interface: str = next(iter(networks_current), None)
+    @staticmethod
+    def _update_network_samples(
+        samples: dict,
+        entity_name: str,
+        formatted_tick: str,
+        current_stat: dict,
+        previous_stat: dict,
+    ) -> None:
+        """
+        Updates network-related samples for a given entity.
 
-                if network_interface and network_interface in networks_previous:
-                    rx_now = networks_current[network_interface].get("rx_bytes", 0)
-                    tx_now = networks_current[network_interface].get("tx_bytes", 0)
+        Args:
+            samples (dict): Dictionary storing collected samples.
+            entity_name (str): The name of the entity being analyzed.
+            formatted_tick (str): The formatted sample tick.
+            current_stat (dict): The current statistics of the entity.
+            previous_stat (dict): The previous statistics of the entity.
+        """
+        networks_current = current_stat.get("networks", {})
+        networks_previous = previous_stat[entity_name].get("networks", {})
 
-                    rx_prev = networks_previous[network_interface].get("rx_bytes", 0)
-                    tx_prev = networks_previous[network_interface].get("tx_bytes", 0)
+        network_interface: str = next(iter(networks_current), None)
 
-                    # Calculate per-tick network usage
-                    rx_usage_samples[docker_entity_name][formatted_tick] = max(
-                        rx_now - rx_prev, 0
-                    ) / (
-                        1024**2
-                    )  # MB
-                    tx_usage_samples[docker_entity_name][formatted_tick] = max(
-                        tx_now - tx_prev, 0
-                    ) / (
-                        1024**2
-                    )  # MB
+        if network_interface and network_interface in networks_previous:
+            rx_now = networks_current[network_interface].get("rx_bytes", 0)
+            tx_now = networks_current[network_interface].get("tx_bytes", 0)
 
-                # Update previous stats **after** all measurements
-                previous_stat[docker_entity_name] = current_stat
+            rx_prev = networks_previous[network_interface].get("rx_bytes", 0)
+            tx_prev = networks_previous[network_interface].get("tx_bytes", 0)
 
-            sample_tick += self.period
-            time.sleep(self.period)
+            samples["rx_usage_samples"][entity_name][formatted_tick] = max(rx_now - rx_prev, 0) / (
+                1024**2
+            )  # MB
+            samples["tx_usage_samples"][entity_name][formatted_tick] = max(tx_now - tx_prev, 0) / (
+                1024**2
+            )  # MB
 
-        # Write results to file if requested
-        if write_to_file:
-            filename: str = f"container_analysis_{datetime.now().strftime('%Y%m%d_%H%M%S')}.json"
-            file_result: dict = {
-                "cpu_cores_samples": cpu_cores_samples,
-                "ram_usage_samples": ram_usage_samples,
-                "disk_usage_samples": disk_usage_samples,
-                "rx_usage_samples": rx_usage_samples,
-                "tx_usage_samples": tx_usage_samples,
-            }
-            with open(filename, "w", encoding="utf-8") as opened_file:
-                json.dump(file_result, opened_file, indent=4)
-            LOGGER.info(f"Results saved to {filename}")
+    @staticmethod
+    def _write_results_to_file(samples: dict) -> None:
+        """
+        Writes collected sample data to a JSON file.
 
+        Args:
+            samples (dict): The collected resource usage samples.
+        """
+        filename: str = f"container_analysis_{datetime.now().strftime('%Y%m%d_%H%M%S')}.json"
+        with open(filename, "w", encoding="utf-8") as opened_file:
+            json.dump(samples, opened_file, indent=4)
+        LOGGER.info(f"Results saved to {filename}")
+
+    def _compute_results(self, samples: dict) -> dict:
+        """
+        Computes the mean resource usage values from the collected samples.
+
+        Args:
+            samples (dict): Dictionary storing collected resource usage samples.
+
+        Returns:
+            dict: Dictionary containing mean resource values and sample data.
+        """
         return {
-            "mean_cpu_cores": self.get_mean_values(cpu_cores_samples),
-            "mean_ram_usage_mb": self.get_mean_values(ram_usage_samples),
-            "mean_disk_usage_mb": self.get_mean_values(disk_usage_samples),
-            "mean_rx_mb": self.get_mean_values(rx_usage_samples),
-            "mean_tx_mb": self.get_mean_values(tx_usage_samples),
-            "samples_cpu": cpu_cores_samples,
-            "samples_ram": ram_usage_samples,
-            "samples_disk": disk_usage_samples,
-            "samples_rx": rx_usage_samples,
-            "samples_tx": tx_usage_samples,
+            "mean_cpu_cores": self.get_mean_values(samples["cpu_cores_samples"]),
+            "mean_ram_usage_mb": self.get_mean_values(samples["ram_usage_samples"]),
+            "mean_disk_usage_mb": self.get_mean_values(samples["disk_usage_samples"]),
+            "mean_rx_mb": self.get_mean_values(samples["rx_usage_samples"]),
+            "mean_tx_mb": self.get_mean_values(samples["tx_usage_samples"]),
+            "samples_cpu": samples["cpu_cores_samples"],
+            "samples_ram": samples["ram_usage_samples"],
+            "samples_disk": samples["disk_usage_samples"],
+            "samples_rx": samples["rx_usage_samples"],
+            "samples_tx": samples["tx_usage_samples"],
         }
 
 
